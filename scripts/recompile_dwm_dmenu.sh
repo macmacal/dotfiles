c16d6e162c43b75a5e98070e98187c0d1073d1e5
macmacale <mac.aleksandrowicz@gmail.com>
2022-05-12 01:07:09 +0200

    Added patch section for dwm

--- scripts/recompile_dwm_dmenu.sh
+++ scripts/recompile_dwm_dmenu.sh
@@ -2,16 +2,16 @@
 
 tmp_dir=$(pwd)
 
+echo " >>>>>>>>>> Recompiling dwmblocks-async..."
+cd $HOME/.local/src/dwmblocks-async
+make clean install
+
 echo " >>>>>>>>>> Recompiling dwm..."
 cd $HOME/.local/src/dwm
 make clean install
 
 echo " >>>>>>>>>> Recompiling dmenu..."
 cd $HOME/.local/src/dmenu
-make clean install
-
-echo " >>>>>>>>>> Recompiling dwmblocks-async..."
-cd $HOME/.local/src/dwmblocks-async
 make clean install
 
 echo "Done"
