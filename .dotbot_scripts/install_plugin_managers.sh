b4059407170f06a12e80fb7132e365f1859eb6d0
macmacal <mac.aleksandrowicz@gmail.com>
2025-09-06 01:49:51 +0200

    removed neovim plugin manager in favour of lazy

--- .dotbot_scripts/install_plugin_managers.sh
+++ .dotbot_scripts/install_plugin_managers.sh
@@ -3,6 +3,3 @@
 # ZSH plugin manager Antidote
 git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
 
-# Neovim-plug plugin manager
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
