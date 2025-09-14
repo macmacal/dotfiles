9087fa3f90af310d4dc2b462edc3a449f7032a9b
macmacal <mac.aleksandrowicz@gmail.com>
2025-09-14 21:12:18 +0200

    ADD: added lua LSP

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "pyright", "clangd" }
+local servers = { "pyright", "clangd", "lua_ls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers
+-- read : vim.lsp.config for changing options of lsp server
