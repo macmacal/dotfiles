ceee1813baa153ba09bad2490ab5d6f167a010b7
macmacal <mac.aleksandrowicz@gmail.com>
2025-09-14 21:33:34 +0200

    DOC: language names in LSP config

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,10 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "pyright", "clangd", "lua_ls" }
+local servers = {
+  "pyright", -- Python
+  "clangd", -- C++
+  "lua_ls"  -- LUA,
+}
 vim.lsp.enable(servers)
 
 -- read : vim.lsp.config for changing options of lsp server
