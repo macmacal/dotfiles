17d05049dc5ad21aa329b7ef674dae523b89884f
Maciej Aleksandrowicz <mac.aleksandrowicz@gmail.com>
2026-06-05 18:12:30 +0200

    nvim - Configured lazygit

---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,37 @@
+return {
+  {
+      "kdheepak/lazygit.nvim",
+      lazy = true,
+      cmd = {
+          "LazyGit",
+          "LazyGitConfig",
+          "LazyGitCurrentFile",
+          "LazyGitFilter",
+          "LazyGitFilterCurrentFile",
+      },
+      -- optional for floating window border decoration
+      dependencies = {
+          "nvim-lua/plenary.nvim",
+      },
+      -- setting the keybinding for LazyGit with 'keys' is recommended in
+      -- order to load the plugin when the command is run for the first time
+      keys = {
+          { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+      }
+  },
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+
+			vim.keymap.set("n", "<leader>gp", "Gitsigns preview_hunk<CR>", {})
+		end,
+	},
+	{
+		"f-person/git-blame.nvim",
+		config = function ()
+			require("gitblame").setup()
+			vim.keymap.set("n", "<leader>gb", ":GitBlameToggle<CR", {})
+		end
+	}
+}
