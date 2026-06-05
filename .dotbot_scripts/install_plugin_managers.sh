f1432c7469c34c7fd624aa9d3b76d9d9b4a13ca6
Maciej Aleksandrowicz <mac.aleksandrowicz@gmail.com>
2026-06-05 17:39:19 +0200

    Added tmux plugin manager (TPM)

--- .dotbot_scripts/install_plugin_managers.sh
+++ .dotbot_scripts/install_plugin_managers.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
+set -e
 
-# ZSH plugin manager Antidote
-git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
+clone_or_update() {
+    repo_dir="$1"
+    repo_url="$2"
 
+    if [ -d "$repo_dir/.git" ]; then
+        echo "Updating $(basename "$repo_dir")..."
+        git -C "$repo_dir" pull --ff-only
+    else
+        echo "Cloning $(basename "$repo_dir")..."
+        git clone --depth=1 "$repo_url" "$repo_dir"
+    fi
+}
+
+clone_or_update "${ZDOTDIR:-$HOME}/.antidote" "https://github.com/mattmc3/antidote.git"
+clone_or_update "$HOME/.tmux/plugins/tpm" "https://github.com/tmux-plugins/tpm.git"
