84e1803705506796e4249cb2e9d8a58e38d13f7e
Maciej Aleksandrowicz <mac.aleksandrowicz@gmail.com>
2026-06-05 17:41:01 +0200

    nvim: Added notify & tmux integration

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -70,6 +70,22 @@ local function my_on_attach(bufnr)
   vim.keymap.set('n', 'ga',    git_add,                           opts('Git Add'))
 end
 
+--- for nvchad users, this is in the ~/.config/nvim/lua/mappings.lua file
+
+local unmap = vim.keymap.del
+
+--- This fixes an issue with the vim-tmux-navigator + nvchad in which the base nvchad
+--- mapping were conflicting with vim-tmux-navigator ones.
+unmap("n", "<c-h>")
+unmap("n", "<c-j>")
+unmap("n", "<c-k>")
+unmap("n", "<c-l>")
+map("n", "<c-h>", "<cmd>:TmuxNavigateLeft<cr>")
+map("n", "<c-j>", "<cmd>:TmuxNavigateDown<cr>")
+map("n", "<c-k>", "<cmd>:TmuxNavigateUp<cr>")
+map("n", "<c-l>", "<cmd>:TmuxNavigateRight<cr>")
+map("n", "<c-\\>", "<cmd>:TmuxNavigatePrevious<cr>")
+
 -- pass to setup along with your other options
 require("nvim-tree").setup {
   on_attach = my_on_attach,
