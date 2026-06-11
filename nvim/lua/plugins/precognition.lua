4f4f2d3b137a65a888a9b80bc5e374d48124a6a8
macmacal <mac.aleksandrowicz@gmail.com>
2026-06-12 00:18:16 +0200

    nvim: wip on precongition

---
+++ nvim/lua/plugins/precognition.lua
@@ -0,0 +1,46 @@
+return {
+    -- "tris203/precognition.nvim",
+    -- event = "VeryLazy",
+    -- lazy = false,
+    -- opts = {
+    -- startVisible = true,
+    -- debounceMs = 0,
+    -- showBlankVirtLine = true,
+    -- highlightFullVirtLine = false,
+    -- highlightColor = { link = "Comment" },
+    -- targetedMotionHighlightColor = { link = "PrecognitionTargetedMotionDefault" },
+    -- textObjectHighlightColors = {
+    --     { link = "DiffText" },
+    --     { link = "DiffChange" },
+    --     { link = "Visual" },
+    -- },
+        -- targetedMotionHints = {
+             -- enabled = false,
+             -- prio = 1,
+        -- },
+      -- hints = {
+    --      Caret = { text = "^", prio = 2 },
+    --      Dollar = { text = "$", prio = 1 },
+    --      MatchingPair = { text = "%", prio = 5 },
+    --      Zero = { text = "0", prio = 1 },
+    --      w = { text = "w", prio = 10 },
+    --      b = { text = "b", prio = 9 },
+    --      e = { text = "e", prio = 8 },
+    --      W = { text = "W", prio = 7 },
+    --      B = { text = "B", prio = 6 },
+    --      E = { text = "E", prio = 5 },
+    -- },
+    -- gutterHints = {
+    --     G = { text = "G", prio = 10 },
+    --     gg = { text = "gg", prio = 9 },
+    --     PrevParagraph = { text = "{", prio = 8 },
+    --     NextParagraph = { text = "}", prio = 8 },
+    -- },
+    -- disabled_fts = {
+    --     "startify",
+    -- },
+    -- },
+    -- config = function()
+      -- require("precognition").toggle()
+    -- end
+}
