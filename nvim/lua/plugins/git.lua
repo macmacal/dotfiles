9113b5547862cf16f09d5658813ff338c2c9acc7
macmacle <mac.aleksandrowicz@gmail.com>
2026-07-20 13:48:22 +0200

    Updated nvim git plugins

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -16,22 +16,20 @@ return {
       -- setting the keybinding for LazyGit with 'keys' is recommended in
       -- order to load the plugin when the command is run for the first time
       keys = {
-          { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+          { "<leader>gl", "<cmd>LazyGit<cr>", desc = "LazyGit" }
       }
   },
 	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-
-			vim.keymap.set("n", "<leader>gp", "Gitsigns preview_hunk<CR>", {})
-		end,
-	},
-	{
 		"f-person/git-blame.nvim",
-		config = function ()
-			require("gitblame").setup()
-			vim.keymap.set("n", "<leader>gb", ":GitBlameToggle<CR", {})
-		end
+    event = "VeryLazy",
+    opts = {
+        enabled = true,
+        message_template = " <summary> • <date> • <author> • <<sha>>",
+        date_format = "%Y-%m-%d %H:%M",
+        virtual_text_column = 1,
+    },
+    keys = {
+          { "<leader>gb", "<cmd>GitBlameToggle<cr>", desc = "GitBlame toggle" }
+    }
 	}
 }
